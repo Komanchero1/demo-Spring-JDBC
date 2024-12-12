965146d2d348ced0270983647af3d9072de7ea23
Dmitriy <160003751+Komanchero1@users.noreply.github.com>
2024-12-12 14:04:33 +0400

    Add files via upload

---
+++ DemoJdbcApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.demo_jdbc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJdbcApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJdbcApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/demo_jdbc_api/repository/ProductRepository.java
@@ -0,0 +1,48 @@
+package org.example.demo_jdbc_api.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+
+@Repository
+public class ProductRepository {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+    public  final String fetchProductSql;
+
+
+    //автоматически создается  спрингом NamedParameterJdbcTemplate и передается в конструктор
+    @Autowired
+    public ProductRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.fetchProductSql = read("fetch_product.sql");
+    }
+
+    //метод для извлечения имени продукта из базы данных на основе переданного имени.
+    public String getProductName(String name){
+        Map<String,Object> params = new HashMap<>();
+        params.put("name",name);
+        return jdbcTemplate.queryForObject(fetchProductSql,params,String.class);//Метод queryForObject выполняет SQL запрос, который хранится в строке fetchProductSql
+    }
+
+    //используется для чтения содержимого файла с SQL запросом и возвращения его в виде строки
+    private static String read(String scriptFileName) {
+        try (InputStream is = new ClassPathResource(scriptFileName).getInputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is))){
+            return bufferedReader.lines().collect(Collectors.joining("\n"));
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
